d5529df451516aedd77f0a4cd92581a9c154c78e
DimaMen265 <menzak87@gmail.com>
2023-09-22 01:16:16 +0200

    The end task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach(galleryItem => {
+    const item = document.createElement("li");
+    item.classList.add("gallery__item");
+    
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.setAttribute("href", galleryItem.original);
+
+    const photo = document.createElement("img");
+    photo.classList.add("gallery__image");
+    photo.setAttribute("src", galleryItem.preview);
+    photo.setAttribute("data-source", galleryItem.original);
+    photo.setAttribute("alt", galleryItem.description);
+
+    link.appendChild(photo);
+    item.appendChild(link);
+    gallery.appendChild(item);
+});
+
+const openLightBox = event => {
+    event.preventDefault();
+    
+    if (event.target.classList.contains("gallery__image")) {
+        const instance = basicLightbox.create(
+            `<img src="${event.target.dataset.source}" alt="${event.target.alt}" />`
+        );
+        instance.show();
+
+        const closeEscape = e => {
+            if (e.key === "Escape") {
+                instance.close();
+                gallery.removeEventListener("keydown", closeEscape);
+            };
+        };
+
+        gallery.addEventListener("keydown", closeEscape);
+    };
+};
+
+gallery.addEventListener("click", openLightBox);
